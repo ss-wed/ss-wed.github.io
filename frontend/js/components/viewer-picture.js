dbd25761829c74dc25dba02c4b81882f808fecea
ikeda <rx4_raffin@yahoo.co.jp>
2019-10-11 18:41:55 +0900

    feat: album修正

--- frontend/js/components/viewer-picture.js
+++ frontend/js/components/viewer-picture.js
@@ -4,28 +4,61 @@ Vue.use(VueLazyload, {
 
 Vue.component('viewer-picture', {
   template: `
-    <v-layout wrap>
-      <v-flex xs4 md2 v-for="i in iamge_length" :key="i" style="height: 100px;">
-        <a>
-          <img v-lazy="images[i-1]" style="width:100%; height:100%; object-fit:cover; padding:1px;" @click="on_img_select"></img>
-        </a>
-      </v-flex>
+    <span>
 
-    <v-dialog v-model="dialog" max-width="290">
-      <v-card>
-        <v-img :src=showing_img></v-img>
-      </v-card>
-    </v-dialog>
+      <v-container>
+        <v-layout wrap>
 
-    </v-layout>
+          <v-flex xs4 md2 v-for="i in iamge_length" :key="i" style="height: 100px;">
+            <a>
+              <img v-lazy="images[i-1]" style="width:100%; height:100%; object-fit:cover; padding:1px;" @click="on_img_select"></img>
+            </a>
+          </v-flex>
+
+        </v-layout>
+      </v-container>
+
+      <div :style="style_overlay" v-if="showing">
+        <img :src="showing_img" :style="style_image" ref="img" id="img"></img>
+        <v-btn icon @click="showing=false" :style="style_xbtn">
+          <v-icon :style="style_icon">close</v-icon>
+        </v-btn>
+      </div>
+
+    </span>
   `,
   data: function () {
     return {
       images: [],
       iamge_length: 0,
       loaded_index: 0,
-      dialog: false,
+      showing: false,
       showing_img: '',
+      style_overlay: {
+        position: 'fixed',
+        top: 0,
+        zIndex: 100,
+        width: '100%',
+        height:'100%',
+        verticalAlign:'middle',
+        background: 'rgba(0,0,0,0.6)',
+      },
+      style_image: {
+        all: 'inherit',
+        objectFit: 'contain',
+        padding: "5%",
+      },
+      style_xbtn: {
+        zIndex: 110,
+        color: 'white',
+        top: (window.innerHeight - 50) + "px",
+        left: (window.innerWidth - 50) + "px",
+      },
+      style_icon: {
+        background: 'lightgray',
+        color: 'black',
+        borderRadius: '15px',
+      }
     }
   },
   mounted() {
@@ -63,8 +96,8 @@ Vue.component('viewer-picture', {
       return src
     },
     on_img_select: function (e) {
-      this.dialog = true
       this.showing_img = e.target.currentSrc
+      this.showing = true
     }
   }
 })
