bbe2ce51a2db6922126e68fb14b7830a343c4e91
ikeda <rx4_raffin@yahoo.co.jp>
2019-10-21 21:45:49 +0900

    fix: ドメイン変更にともなうurl修正

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## 開発用フロントエンドサーバー起動
 
-- python -m http.server
+- python -m http.server --directory C:\Users\ikeda\Documents\project\wedding
 
 ## 開発用バックエンドライブラリインストール
 
--- frontend/js/components/header-menu.js
+++ frontend/js/components/header-menu.js
@@ -4,23 +4,23 @@ Vue.component('header-menu', {
       <v-toolbar color="white" height="50px" flat app style="opacity:0.5">
         <v-spacer></v-spacer>
 
-        <v-btn icon @click="on_click_linkbutton('/wedding/index.html')">
+        <v-btn icon @click="on_click_linkbutton('/index.html')">
           <v-icon color="teal">home</v-icon>
         </v-btn>
 
-        <v-btn icon @click="on_click_linkbutton('/wedding/frontend/templates/profile.html')">
+        <v-btn icon @click="on_click_linkbutton('/frontend/templates/profile.html')">
           <v-icon color="teal">supervisor_account</v-icon>
         </v-btn>
 
-        <v-btn icon @click="on_click_linkbutton('/wedding/frontend/templates/form.html')">
+        <v-btn icon @click="on_click_linkbutton('/frontend/templates/form.html')">
           <v-icon color="teal">edit</v-icon>
         </v-btn>
 
-        <v-btn icon @click="on_click_linkbutton('/wedding/frontend/templates/album.html')">
+        <v-btn icon @click="on_click_linkbutton('/frontend/templates/album.html')">
           <v-icon color="teal">photo</v-icon>
         </v-btn>
 
-        <v-btn icon @click="on_click_linkbutton('/wedding/frontend/templates/movie.html')">
+        <v-btn icon @click="on_click_linkbutton('/frontend/templates/movie.html')">
           <v-icon color="teal">movie</v-icon>
         </v-btn>
 
